923c707476b7e5d11d8024be445f43c07d347c52
Manish Kumar Gupta <manishgu231@gmail.com>
2025-01-19 00:58:55 +0530

    DEBUG : in prod(redis connection issue)

--- src/app/lib/redis.ts
+++ src/app/lib/redis.ts
@@ -1,10 +1,32 @@
 import Redis from "ioredis";
-console.log(process.env.REDIS_PORT);
+
+// Log the environment variables to verify that they are being loaded correctly
+console.log("Redis Host:", process.env.REDIS_HOST);
+console.log("Redis Port:", process.env.REDIS_PORT);
+
+const redisPort = process.env.REDIS_PORT
+  ? Number(process.env.REDIS_PORT)
+  : 6379;
+
+// Log to check if redisPort is a valid number
+if (isNaN(redisPort) || redisPort < 0 || redisPort >= 65536) {
+  console.error(`Invalid Redis port: ${process.env.REDIS_PORT}`);
+  throw new Error(`Invalid Redis port: ${process.env.REDIS_PORT}`);
+}
+
 const redis = new Redis({
   host: process.env.REDIS_HOST!, // Replace with your Redis host
-  port: Number(process.env.REDIS_PORT!), // Default Redis port
+  port: redisPort, // Default Redis port
   password: process.env.REDIS_PASSWORD!, // Optional password
   db: 0, // Default DB (you can change it if needed)
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
+
+redis.on("connect", () => {
+  console.log("Connected to Redis");
 });
 
 export default redis;
